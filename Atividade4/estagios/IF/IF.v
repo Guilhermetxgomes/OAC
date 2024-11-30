cd203b72ad556159a01747e8b944e1fd79bb0e04
Guilherme Gomes <guilhermetexgomes@gmail.com>
2024-11-30 15:23:20 -0300

    feat: Estrutura inical de pastas e IF sem tb

---
+++ Atividade4/estagios/IF/IF.v
@@ -0,0 +1,17 @@
+module fetch (
+  input clock,
+  input reset,
+  input [31:0] pc_branch_value,
+  input mux_sel,
+  input load_pc,
+  input load_if_id_register,
+  output [31:0] pc_out,
+  output [31:0] instruction
+);
+
+
+  
+
+
+
+endmodule
